bdc19a478f118397b407cbb20038c4eb1d4a6df1
Daniil <dania118118@gmail.com>
2024-03-07 18:40:18 +0200

    implemented serial connection

--- laserControl/MainForm.cs
+++ laserControl/MainForm.cs
@@ -4,9 +4,50 @@ namespace laserControl
 {
     public partial class MainForm : Form
     {
+        private SerialPort serialPort;
+
+        private void MessageOnError(Action method)
+        {
+            try { method(); }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
         public MainForm()
         {
+            serialPort = new SerialPort();
             InitializeComponent();
+            initializeControls();
+        }
+
+        private void initializeControls()
+        {
+            connectionButton.Click += (object? sender, EventArgs e) =>
+            {
+                MessageOnError(() =>
+                {
+                    if (serialPort.IsOpen)
+                    {
+                        serialPort.Close();
+                        connectionButton.Text = "Connect";
+                    }
+                    else
+                    {
+                        serialPort.Open();
+                        connectionButton.Text = "Disconnect";
+                    }
+                });
+            };
+
+            serialPortSelector.DropDown += (object? sender, EventArgs e) =>
+            {
+                serialPortSelector.Items.Clear();
+                serialPortSelector.Items.AddRange(SerialPort.GetPortNames());
+            };
+
+            serialPortSelector.SelectedIndexChanged += (object? sender, EventArgs e) =>
+            {
+                serialPort.PortName = serialPortSelector.SelectedItem?.ToString();
+            };
         }
     }
 }
