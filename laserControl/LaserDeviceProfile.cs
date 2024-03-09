76bbc9e6f37823f29abf4002cce71f78eb84f0fb
Daniil <dania118118@gmail.com>
2024-03-09 13:39:36 +0200

    implemented LaserDeviceProfile

---
+++ laserControl/LaserDeviceProfile.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+
+namespace laserControl
+{
+    public class LaserDeviceProfile
+    {
+        //properties of physical device installation that affect only PC-side computations
+        public float ScreenSize { get => screensSize; set => screensSize = numberPositivenessCheck(value); } //meters
+        public float ScreenDistance { get => screenDistance; set => screenDistance = numberPositivenessCheck(value); } //meters
+        public float MirrorDistance { get => mirrorDistance; set => mirrorDistance = numberPositivenessCheck(value); } //meters
+        public UInt32 MotorStepsPerRotation { get; set; } = 4096; //motor steps
+        public bool IsAxisXInverted { get; set; } = false;
+        public bool IsAxisYInverted { get; set; } = false;
+        public UInt32 MaxSpeed { get; set; } = 100; //motors steps per second
+        //device-side properties
+        public UInt16 AxisXBacklash { get; set; } = 0; //motor steps
+        public UInt16 AxisYBacklash { get; set; } = 0; //motor steps
+
+        public Vector2 AngularMotorsPosition(Vector2 laserPosition)
+        {
+            static double transform(double x) => x + ((x < 0) ? Math.PI / 2 : 0);
+            double beta = transform(Math.Atan(ScreenDistance / laserPosition.Y) / 2);
+            double alpha = transform(Math.Atan((ScreenDistance / Math.Sin(2 * beta) + MirrorDistance) / laserPosition.X) / 2);
+            return new Vector2((float)alpha, (float)beta);
+        }
+
+        public Int16[] ToMotorSteps(Vector2 motorAngle)
+        {
+            var origin = AngularMotorsPosition(new Vector2(0, 0));
+            var result = (motorAngle - origin) * (float)(MotorStepsPerRotation / (Math.PI * 2));
+            return [Convert.ToInt16(IsAxisXInverted ? -result.X : result.X), Convert.ToInt16(IsAxisYInverted ? -result.Y : result.Y)];
+        }
+
+        private float numberPositivenessCheck(float value)
+        {
+            if (value <= 0) throw new ArgumentException("the value must be bigger than zero");
+            return value;
+        }
+
+        private float screensSize = 1;
+        private float screenDistance = 1;
+        private float mirrorDistance = 0.1f;
+    }
+}
