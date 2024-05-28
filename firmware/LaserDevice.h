698b8a45329521ed1b1bc1d7d6212d249c00492d
Daniil <dania118118@gmail.com>
2024-05-28 22:45:39 +0300

    encapsulate firmware functionality into LaserDevice class

---
+++ firmware/LaserDevice.h
@@ -0,0 +1,90 @@
+#ifndef _LASER_DEVICE_h
+#define _LASER_DEVICE_h
+
+#include <stdint.h>
+#include <FIFO.h>
+
+#include <GyverPlanner2.h>
+
+
+template <GS_driverType driverType, OnTarget onTarget, OnTarget onTargetInternalFunc>
+class LaserDevice {
+public:
+	using Planner = GPlanner2<driverType, 2, onTargetInternalFunc>;
+
+private:
+//	using Planner = GPlanner2<driverType, 2, onTargetLocal>
+	using Intensities = FIFO<uint8_t, 32>;
+
+	Planner& planner;
+	Intensities intensities {};
+	int laserPin;
+	bool isStopping = false;
+	uint8_t lastIntensity = 0;
+
+	void stopToClearBuffer() {
+		bool stopped = planner.getStatus() < 2;
+		if (stopped) {
+			clearBufferOnStop();
+		} else {
+			isStopping = true;
+			planner.stop();
+		}
+	}
+
+	void clearBufferOnStop() {
+		planner.brake();
+		planner.clearBuffer();
+		intensities.clear();
+		int32_t currentPos[] = {planner.getCurrent(0), planner.getCurrent(1)};
+		planner.addTarget(currentPos, 0);
+		planner.start();
+	}
+
+public:
+	void onTargetInternal() {
+		lastIntensity = intensities.get();
+		analogWrite(laserPin, lastIntensity);
+		intensities.next();
+		onTarget();
+	}
+
+	LaserDevice(Planner& planner, int laserPin) :
+		planner {planner},
+		laserPin {laserPin}
+	{
+		int16_t startPoint[2] {0, 0};
+		planner.setCurrent(startPoint);
+		planner.setAcceleration(1000);
+		planner.setMaxSpeed(1);
+		planner.start();
+	}
+
+	void setMaxSpeed(double speed) {
+		planner.setMaxSpeed(speed);
+	}
+
+	void setBacklash(uint8_t axis, uint16_t steps) {
+		planner.setBacklash(axis, steps);
+	}
+
+	void addTarget(int16_t position[], uint8_t intensity) {
+		planner.addTarget(position, 0);
+		intensities.add(intensity);
+	}
+
+	void clearBuffer() {
+		stopToClearBuffer();
+	}
+
+	void tick() {
+		bool isStopped = planner.getStatus() < 2;
+		if (isStopping && isStopped) {
+			isStopping = false;
+			clearBufferOnStop();
+		}
+		planner.tick();
+	}
+};
+
+#endif
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,6 +2,7 @@
 #include <FIFO.h>
 #include "LaserDeviceMessage.h"
 #include "IOPort.h"
+#include "LaserDevice.h"
 
 constexpr int laserPin = 10;
 constexpr int baudRate = 9600;
@@ -9,84 +10,58 @@ constexpr auto driverType = STEPPER4WIRE_HALF; //STEPPER2WIRE, STEPPER4WIRE, STE
 
 void processMessage(LaserDeviceMessage);
 void onTarget();
+void onTargetInternal();
 
-using Planner = GPlanner2<driverType, 2, onTarget>;
 using StepperT = Stepper<driverType>;
-using Intensities = FIFO<uint8_t, 32>;
 
 IOPort<LaserDeviceMessage> ioPort {processMessage};
-Intensities intensities {};
+using Device = LaserDevice<driverType, onTarget, onTargetInternal>;
 
-Planner planner;
+Device::Planner planner;
 StepperT stepper0(5, 3, 4, 2);
 StepperT stepper1(9, 7, 8, 6);
 
-uint8_t lastIntensity = 0;
-bool isStopping = false;
+Device laserDevice {planner, laserPin};
 
-void initializePlanner(Planner& planner) {
+void initializePlanner(Device::Planner& planner) {
 	planner.addStepper(0, stepper0);
 	planner.addStepper(1, stepper1);
-	int16_t startPoint[2] {0, 0};
-	planner.setCurrent(startPoint);
-	planner.setAcceleration(1000);
-	planner.setMaxSpeed(1);
-	planner.start();
 }
 
 void onTarget() {
-	lastIntensity = intensities.get();
-	analogWrite(laserPin, lastIntensity);
-	intensities.next();
 	LaserDeviceMessage message;
 	message.type = MessageType::TargetReached;
 	ioPort.send(message);
 }
 
-void stopToClearBuffer() {
-	bool stopped = planner.getStatus() < 2;
-	if (stopped) {
-		clearBufferOnStop();
-	} else {
-		isStopping = true;
-		planner.stop();
-	}
-}
-
-void clearBufferOnStop() {
-	planner.brake();
-	planner.clearBuffer();
-	intensities.clear();
-	int32_t currentPos[] = {planner.getCurrent(0), planner.getCurrent(1)};
-	planner.addTarget(currentPos, 0);
-	planner.start();
+void onTargetInternal() {
+	laserDevice.onTargetInternal();
 }
 
 void processMessage(LaserDeviceMessage message) {
 	auto& data = message.data;
 	switch (message.type) {
 	case MessageType::SetSpeed:
-		planner.setMaxSpeed(data.speedData.speed);
+		laserDevice.setMaxSpeed(data.speedData.speed);
 		break;
 	case MessageType::SetBacklashX:
-		planner.setBacklash(0, data.backlashData.backlash);
+		laserDevice.setBacklash(0, data.backlashData.backlash);
 		break;
 	case MessageType::SetBacklashY:
-		planner.setBacklash(1, data.backlashData.backlash);
+		laserDevice.setBacklash(1, data.backlashData.backlash);
 		break;
 	case MessageType::AddTarget:
-		planner.addTarget(data.targetData.position, 0);
-		intensities.add(data.targetData.intensity);
+		laserDevice.addTarget(data.targetData.position, data.targetData.intensity);
 		break;
 	case MessageType::ClearBuffer:
-		stopToClearBuffer();
+		laserDevice.clearBuffer();
 		break;
 	case MessageType::Echo:
 		message.type = MessageType::Echo;
 		ioPort.send(message);
 		break;
 	case MessageType::ResetOrigin:
-		planner.reset();
+//		planner.reset();
 		break;
 	}
 }
@@ -98,13 +73,6 @@ void setup() {
 }
 
 void loop() {
-	bool isStopped = planner.getStatus() < 2;
-	if (isStopping && isStopped) {
-		isStopping = false;
-		clearBufferOnStop();
-	}
-	if (!isStopping) {
-		ioPort.tick();
-	}
-	planner.tick();
+	ioPort.tick();
+	laserDevice.tick();
 }
